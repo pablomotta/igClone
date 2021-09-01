1cbf3dba771f0847d193b0c083869fc418bb3b43
Pablo Motta <pmotta.dev@gmail.com>
2021-09-01 13:48:36 -0700

    dynamic form footer

---
+++ client/src/components/AuthCard/FormFooter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './style.scss';
+
+const FormFooter = (props) => {
+	const { authType, setAuthType } = props;
+	return (
+		<div className='form-footer'>
+			<div className='form-footer-text text-center'>
+				{authType !== 'login' ? (
+					<p>
+						Have an account?{' '}
+						<span onClick={() => setAuthType('login')}>Login</span>
+					</p>
+				) : (
+					<p>
+						Don't Have an account?{' '}
+						<span onClick={() => setAuthType('register')}>
+							Sign Up
+						</span>{' '}
+					</p>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default FormFooter;
