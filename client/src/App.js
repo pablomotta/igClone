e8ae6ff29e267dfea4fc66ab5014f8d8f7e7aacd
Pablo Motta <pmotta.dev@gmail.com>
2021-08-31 21:27:47 -0700

    frontend boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// import PrivateRoute from './components/routing/PrivateRoute';
+import Home from './components/pages/Home/index.js';
+import Landing from './components/pages/Landing/index.js';
+import Alerts from './components/Alerts';
+import Footer from './components/Footer';
+
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
+import './App.scss';
+
+if (localStorage.token) {
+	setAuthToken(localStorage.token);
+}
+
+const App = () => {
+	return (
+		<AuthState>
+			<AlertState>
+				<Router>
+					<Fragment>
+						<Alerts />
+						<main className='container'>
+							<Switch>
+								<Route exact path='/' component={Home} />
+								<Route
+									exact
+									path='/login'
+									component={Landing}
+								/>
+							</Switch>
+						</main>
+						<Footer />
+					</Fragment>
+				</Router>
+			</AlertState>
+		</AuthState>
+	);
+};
+
+export default App;
---
+++ client/src/components/pages/Landing/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.scss';
+
+const Landing = () => {
+	return (
+		<div>
+			<h1>Landing Page</h1>
+			<p>test</p>
+		</div>
+	);
+};
+
+export default Landing;
