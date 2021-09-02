2439f353f6cac90fdd6506c9277fc8e1532eb180
Pablo Motta <pmotta.dev@gmail.com>
2021-09-02 15:19:13 -0700

    import styles

--- client/src/components/Alerts/index.js
+++ client/src/components/Alerts/index.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import AlertContext from '../../context/alert/alertContext';
+import './style.scss';
 
 const Alerts = () => {
 	const alertContext = useContext(AlertContext);
@@ -7,7 +8,7 @@ const Alerts = () => {
 		alertContext.alerts.length > 0 &&
 		alertContext.alerts.map((alert) => (
 			<div key={alert.id} className={`alert alert-${alert.type}`}>
-				<i className='fas fa-info-circle' /> {alert.msg}
+				{alert.msg}
 			</div>
 		))
 	);
