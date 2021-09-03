5080327234859db996acdb11c0e73bbc08370e30
Pablo Motta <pmotta.dev@gmail.com>
2021-09-03 16:21:31 -0700

    slider v1

--- client/src/components/pages/Landing/index.js
+++ client/src/components/pages/Landing/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 import './style.scss';
-import phones from './phones.png';
+// images
+import phones from './img/phones.png';
+import bg1 from './img/ig-1.jpeg';
+import bg2 from './img/ig-2.jpeg';
+import bg3 from './img/ig-3.jpeg';
+import bg4 from './img/ig-4.jpeg';
+import bg5 from './img/ig-5.jpeg';
 
 import AuthCard from '../../AuthCard';
 
@@ -10,6 +16,13 @@ const Landing = () => {
 		<article className='container-landing'>
 			<div className='container-image'>
 				<img className='phones' src={phones} alt='phones' />
+				<div className='image-backgrounds' id='slider'>
+					<img src={bg1} alt='background 1' />
+					<img src={bg2} alt='background 2' />
+					<img src={bg3} alt='background 3' />
+					<img src={bg4} alt='background 4' />
+					<img src={bg5} alt='background 5' />
+				</div>
 			</div>
 			<AuthCard />
 		</article>
